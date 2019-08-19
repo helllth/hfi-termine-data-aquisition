8d51f09c0836d2d420d5cdeeac47d47b9610b62d
helllth <th@cismet.de>
2019-08-19 13:30:35 +0200

    switched to sync file writing

--- tools.js
+++ tools.js
@@ -1,7 +1,13 @@
 import fs from 'fs-extra';
 import md5 from 'md5';
-
-export const writeFileWithMD5 = (name, data, encoding, finished) => {
-	fs.writeFile(name, data, encoding, finished);
-	fs.writeFile(name + '.md5', md5(data), encoding, () => {});
+() => console.log(`leaderboard.${team}.json geschrieben`);
+export const writeFileWithMD5 = (
+	name,
+	data,
+	encoding = 'utf8',
+	finished = () => console.log(`${name} geschrieben`)
+) => {
+	fs.writeFileSync(name + '.md5', md5(data), { encoding });
+	fs.writeFileSync(name, data, { encoding });
+	finished();
 };
